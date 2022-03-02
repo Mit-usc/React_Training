3c0a8565904b37b96ca645fbf070e53adabee8f3
Mit-usc <98483112+Mit-usc@users.noreply.github.com>
2022-03-02 13:41:20 -0800

    feat(Week 6 React): Add/Edit/Delete Finished adding "add" functionality as well as edit/delete
    ICTRAINING-1100

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,12 +9,49 @@ let id = 0;
 
 function App() {
   const [listItem, setItem] = useState(0);
-  console.log(listItem);
-
   const [list, setList] = useState([]);
 
+  // Function to add a specific to-do task to current list
   const onAdd = (toDo) => {
     setList((prevVal) => [...prevVal, { id: (id += 1), toDo: toDo }]);
+  };
+
+  // Function to edit a specific to-do task
+  const onEdit = (editData) => {
+    var index = 0;
+
+    // Loops through all entries within to-do list and edits the specific user-inputted index
+    var newArr = Object.entries(list).map(([key, value]) => {
+      index++;
+      // eslint-disable-next-line eqeqeq
+      if (value.id == editData.id) {
+        return { id: index, toDo: editData.item };
+      }
+      return { id: index, toDo: value.toDo };
+    });
+    setList(newArr);
+  };
+
+  // Function to delete a specific to-do task
+  const onDelete = (indexDelete) => {
+    var index = 0;
+
+    // Loops through all entries within to-do list and only adds non-to-delete values
+    var newArr = Object.entries(list).map(([key, value]) => {
+      index++;
+      // eslint-disable-next-line eqeqeq
+      if (value.id == indexDelete.id2) {
+        index--;
+        return false;
+      } else {
+        return { id: index, toDo: value.toDo };
+      }
+    });
+
+    // removes false value from array generated above (logic should be better optimized here)
+    const newArr2 = newArr.filter((val) => val !== false);
+    console.log(newArr2);
+    setList(newArr2);
   };
 
   return (
@@ -24,6 +61,7 @@ function App() {
       <br></br>
       <BasicTable item={listItem} data={list} />
       <ToDo addItem={onAdd} />
+      <Edit editItem={onEdit} deleteItem={onDelete} />
 
       {/* Routing set-up, allows url switches based on paths below */}
       <Routes>
--- my-app/src/Edit.js
+++ my-app/src/Edit.js
@@ -1,9 +1,61 @@
 import "./App.css";
+import { useForm } from "react-hook-form";
 
-function Edit() {
+function Edit(props) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  // Moves to-edit data to parent (App.js)
+  const onSubmit = (data) => {
+    props.editItem(data);
+  };
+
+  // Moves to-delete data to parent (App.js)
+  const onSubmit2 = (data) => {
+    props.deleteItem(data);
+  };
+
   return (
     <div>
-      <h1>Edit/Delete a To-Do item</h1>
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Edit an item</h1>
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input
+          defaultValue="Enter Index"
+          {...register("id", { required: true })}
+        />
+        <input
+          defaultValue="Enter To-Do Item"
+          {...register("item", { required: true })}
+        />
+        <input type="submit" />
+        <br></br>
+
+        {/* errors will return when field validation fails  */}
+        {errors.id && <span>This field is required</span>}
+      </form>
+
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit2)}>
+        <h1>Delete an item</h1>
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input
+          defaultValue="Enter Index"
+          {...register("id2", { required: true })}
+        />
+        <input type="submit" />
+        <br></br>
+
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+      </form>
     </div>
   );
 }
